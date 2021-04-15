2cc9cb7f84aa20446944eb60b7bb6e101583abfe
MaioranoB <bernardo.maiorano@hotmail.com>
2021-04-15 11:59:02 -0300

    add c implementation

---
+++ exercise01/Makefile
@@ -0,0 +1,17 @@
+# the compiler
+CC = gcc
+
+# compiler flags:
+#  -Wall turns on most, but not all, compiler warnings
+CFLAGS  = -Wall
+
+# the build target executable:
+TARGET = main
+
+all: $(TARGET)
+
+$(TARGET): $(TARGET).c
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c
+
+clean:
+	$(RM) $(TARGET)
---
+++ exercise01/main.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+double *create_rand_array(int n);
+double *create_empty_array(int n);
+double **create_rand_square_matrix(int n);
+double time_to_multiply_ij(int n, double **matrix, double *array, double *result);
+double time_to_multiply_ji(int n, double **matrix, double *array, double *result);
+
+int main(void){
+    srand(time(NULL));
+
+    // b = A * x
+    double **A;
+    double *x;
+    double *b; 
+
+    FILE *csv_file = fopen("c_times.csv", "w");
+    fprintf(csv_file,"n,Time_ij,Time_ji\n");
+    
+    int n,k;
+    int range = 16;
+    for (int i = 0; i <= range; i++){
+        n = i * 1000;
+        
+        A = create_rand_square_matrix(n); 
+        x = create_rand_array(n);
+        b = create_empty_array(n);
+        
+        double time_ji = time_to_multiply_ji(n,A,x,b);
+        double time_ij = time_to_multiply_ij(n,A,x,b);
+        
+        printf("n = %5d -> ij: %.6f / ji: %.6f \n",n,time_ij,time_ji);
+        fprintf(csv_file,"%d,%.6f,%.6f\n",n, time_ij, time_ji);
+
+        for (k = 0; k < n; k++){
+            free(A[k]);
+        }
+        free(A);
+        free(x);
+        free(b);
+    }
+
+    fclose(csv_file);
+    return 0;
+}
+
+double *create_rand_array(int n){
+    double *array = (double *)malloc(n * sizeof(double));
+    for (int i = 0; i < n; i++){
+        array[i] = (double)rand();
+    }
+    return array;
+}
+double *create_empty_array(int n){
+    double *array = (double *)malloc(n * sizeof(double));
+    for (int i = 0; i < n; i++){
+        array[i] = 0;
+    }
+    return array;
+}
+double **create_rand_square_matrix(int n){
+    double **matrix = (double **)malloc(n * sizeof(double*));
+    for (int i = 0; i < n; i++){
+        matrix[i] = create_rand_array(n);
+    }
+    return matrix;
+}
+
+double time_to_multiply_ij(int n, double **matrix, double *array, double *result){
+    double s;
+    clock_t start = clock();
+    for (int i = 0; i < n; i++){
+        s = 0;
+        for (int j = 0; j < n; j++){
+            // result[i] += matrix[i][j] * array[j];
+            s += matrix[i][j] * array[j];
+        }
+        result[i] = s;
+    }
+    clock_t end = clock();
+    return (double)(end - start)/CLOCKS_PER_SEC;
+}
+double time_to_multiply_ji(int n, double **matrix, double *array, double *result){
+    double t;
+    clock_t start = clock();
+    for (int j = 0; j < n; j++){
+        t = array[j];
+        for (int i = 0; i < n; i++){
+            // result[i] += matrix[i][j] * array[j];
+            result[i] += matrix[i][j] * t;
+        }
+    }
+    clock_t end = clock();
+    return (double)(end - start)/CLOCKS_PER_SEC;
+}
