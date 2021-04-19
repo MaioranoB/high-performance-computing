d4771b8ebcd4ea90fb01de233fee1fdbb36262af
MaioranoB <bernardo.maiorano@hotmail.com>
2021-04-19 11:35:02 -0300

    improving graph

--- exercise01/make_graph.py
+++ exercise01/make_graph.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 cDF = pd.read_csv('c_times.csv')
 fDF = pd.read_csv('fortran_times.csv')
 
-fig, axs = plt.subplots(1,2,figsize=(10,4), sharey=True)
+fig, axs = plt.subplots(1,2,figsize=(12,4), sharey=False)
 
 cDF.plot.line(x = 'n', y = 'Time_ij', ax = axs[0], ylabel='Time (seconds)')
 cDF.plot.line(x = 'n', y = 'Time_ji', ax = axs[0])
 axs[0].set_title('C',fontsize=15)
 
-fDF.plot.line(x = 'n', y = 'Time_ij', ax = axs[1])
+fDF.plot.line(x = 'n', y = 'Time_ij', ax = axs[1], ylabel='Time (seconds)')
 fDF.plot.line(x = 'n', y = 'Time_ji', ax = axs[1])
 axs[1].set_title('Fortran',fontsize=15)
 
